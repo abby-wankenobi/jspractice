a1d14d5203142e596a1113604ffe0ef78d9bac5b
Abigail Swarth <abigailswarth@gmail.com>
2018-06-18 19:05:51 -0400

    comments

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
-function waitThreeSeconds() {
-  var ms = 3000 + new Date().getTime();
-  while (new Date() < ms){}
-  console.log('finished function')
-}
+// function waitThreeSeconds() {
+//   var ms = 3000 + new Date().getTime();
+//   while (new Date() < ms){}
+//   console.log('finished function')
+// }
+//
+// function clickHandler() {
+//   console.log('click event');
+// }
+//
+// document.addEventListener('click', clickHandler);
+//
+// waitThreeSeconds();
+// console.log('finished execution')
 
-function clickHandler() {
-  console.log('click event');
-}
 
-document.addEventListener('click', clickHandler);
+// var objectLiteral = {
+//   firstname: "mary",
+//   isAProgrammer: true
+// }
+//
 
-waitThreeSeconds();
-console.log('finished execution')
+
+
+// console.log(JSON.stringify(objectLiteral));
+//
+// var jsonValue = JSON.parse('{ "firstname": "mary", "isAProgrammer": true}')
+//
+// console.log(jsonValue.firstname)
+
+
+
+// greet();
+//
+// function greet() {
+//   console.log('hi')
+// }
+//
+// var anonymousGreet = function() {
+//   console.log('hi');
+// }
+//
+// anonymousGreet();
+//
+// function log(a) {
+//   a();
+// }
+//
+// log(function() {
+//   console.log('hi')
+// })
